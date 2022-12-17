0ecb8628bf6a3e8b10eb67bb27df1cbe3038655d
nayera88 <74667782+nayera88@users.noreply.github.com>
2022-12-17 20:16:40 +0200

    first commit

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -7,7 +7,7 @@ import {HttpClient} from '@angular/common/http';
 export class ServiceService {
 
   constructor( private http:HttpClient) { }
-  private url ='http://172.30.126.68:8080/';
+  private url ='https://tools-git-tripproject.apps.eu410.prod.nextcle.com/';
 
   SignUp(user:any){
     return this.http.post(this.url + 'api/admin/signUp' , user);
